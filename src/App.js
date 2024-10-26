c464cfc85afc829842efc7823301d390509603ec
Christianna A. <c.anastasiadou@hotmail.com>
2024-10-26 21:31:28 +0300

    Created simple pokemon app :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+
+const pokemonURL = "https://pokeapi.co/api/v2/pokemon/"
 
 function App() {
+  const [currentPokemon, setCurrentPokemon] = useState();
+  const [currentPokemonIndex, setCurrentPokemonIndex] = useState(1);
+
+  function increasePokemonIndex() {
+    const newIndex = currentPokemonIndex + 1;
+    setCurrentPokemonIndex(newIndex);
+  }
+
+  function decreasePokemonIndex() {
+    if (currentPokemonIndex > 1) {
+      const newIndex = currentPokemonIndex - 1;
+      setCurrentPokemonIndex(newIndex);
+    }
+  }
+
+  useEffect(() => {
+    const fetchPokemon = async () => {
+      const response = await fetch(`${pokemonURL}${currentPokemonIndex}`);
+      const data = await response.json();
+      setCurrentPokemon(data);
+      console.log(data);
+    }
+    fetchPokemon();
+  }, [currentPokemonIndex])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{currentPokemon?.name}</h1>
+      <img src={currentPokemon?.sprites.front_default} width={150}></img>
+      <br />
+      <button onClick={decreasePokemonIndex}>Prev</button>
+      <button onClick={increasePokemonIndex}>Next</button>
     </div>
   );
 }
